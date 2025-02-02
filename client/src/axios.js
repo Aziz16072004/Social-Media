9589123ad097fa61ac6305815ae59115ad3646d9
Aziz16072004 <mouhamedazizchaabani@gmail.com>
2025-02-02 20:16:46 +0100

    some changing  for uploading

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL = process.env.NODE_ENV === 'production'
-  ? 'https://social-media-2-o8uj.onrender.com'
+  ? 'https://social-media-2-o8uj.onrender.com/api'
   : 'http://localhost:8000';
 
 axios.defaults.baseURL = baseURL;
