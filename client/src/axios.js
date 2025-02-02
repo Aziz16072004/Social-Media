5fa0076caa7e5b8de3f1edae279e760844bd2b4e
Aziz16072004 <mouhamedazizchaabani@gmail.com>
2025-02-02 12:43:09 +0100

    ..

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL = process.env.NODE_ENV === 'production'
   ? 'https://social-media-2-o8uj.onrender.com'
-  : 'http://localhost:8000';
+  : 'http://localhost:10000';
 
 axios.defaults.baseURL = baseURL;
 
