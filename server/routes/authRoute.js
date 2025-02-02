383f688414cfc1d6a8a99611bc0991c2960a354e
Aziz16072004 <mouhamedazizchaabani@gmail.com>
2025-02-02 21:38:23 +0100

    ..

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,12 +1,6 @@
 const express = require('express')
 const userController  = require('../controller/userController')
 const router = express.Router()
-
-
-router.get("/signin1" ,(req, res) => {
-    console.log("This is the sign-in API");
-    res.send("Sign-in API hit successfully");
-});
 router.post("/signin" , userController.checkUser)
 router.post("/signup" , userController.Add_InsertUser)
 
