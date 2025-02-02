c49c73b410143c5b297bd97cea440cc2ea717d42
Aziz16072004 <mouhamedazizchaabani@gmail.com>
2025-02-02 11:36:38 +0100

    update

--- server/app.js
+++ server/app.js
@@ -53,7 +53,7 @@ if (process.env.NODE_ENV === "production") {
 mongoose.set("strictQuery" , false);
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb://localhost:27017/data");
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host} `);
     } catch (error) {
         console.log(error);
