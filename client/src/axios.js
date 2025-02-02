ef93826374b5f3d5a30e5e11aecbccbbc48a5983
Aziz16072004 <mouhamedazizchaabani@gmail.com>
2025-02-02 13:10:49 +0100

    update Baseurl

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL = process.env.NODE_ENV === 'production'
-  ? 'http://localhost:10000'
+  ? 'https://social-media-2-o8uj.onrender.com'
   : 'http://localhost:8000';
 
 axios.defaults.baseURL = baseURL;
