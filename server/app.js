61c1b818ae54393bcf801e337aff9bcd1dd8ec86
Aziz16072004 <mouhamedazizchaabani@gmail.com>
2025-02-02 20:23:57 +0100

    update

--- server/app.js
+++ server/app.js
@@ -35,16 +35,17 @@ app.use("/message" , require("./routes/messageRoute"))
 // --------------------------deployment------------------------------
 const __dirname1 = path.resolve();
 
+// Define API routes before serving React
+app.get("/api", (req, res) => {
+  res.send("API is running..");
+});
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/client/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
   );
-} else {
-  app.get("/api", (req, res) => {
-    res.send("API is running..");
-  });
 }
 
 
