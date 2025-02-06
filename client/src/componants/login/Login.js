17ad631d99282a3093964065719a73d87f6aea47
Aziz16072004 <mouhamedazizchaabani@gmail.com>
2025-02-06 10:45:31 +0100

    changin design for sign in & sign up

--- client/src/componants/login/Login.js
+++ client/src/componants/login/Login.js
@@ -3,6 +3,8 @@ import { useNavigate, Link } from "react-router-dom";
 import "./login.css";
 import { useForm } from "react-hook-form";
 import Cookies from 'js-cookie';
+import SocialLogin from './SocialLogin';
+import InputField from './InputField';
 
 function Login() {
   const navigate = useNavigate();
@@ -10,7 +12,7 @@ function Login() {
   const { register, handleSubmit, setError, formState: { errors } } = useForm();
 
   async function onSubmit(data) {
-    // Basic validation
+    console.log(data);
     if (!data.email) {
       setError("email", { type: "manual", message: "Email is required" });
       return;
@@ -44,22 +46,28 @@ function Login() {
   }
 
   return (
-    <div className="login">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <h1>Login</h1>
-        <p>
-          email:
-          <input type="email" {...register("email")} />
-        </p>
-        {errors.email && <p className="errorMessage">{errors.email.message}</p>}
-        <p>
-          password:
-          <input type="password" {...register("password")} />
-        </p>
+    <div className='loginPage'>
+      <div className="login-container">
+      <h2 className="form-title">Log in with</h2>
+      <SocialLogin />
+      <p className="separator"><span>or</span></p>
+      <form action="#" className="login-form" onSubmit={handleSubmit(onSubmit)}>
+      <InputField type="email" placeholder="Email address" icon="mail"  register={register("email")}  />
+          {errors.email && <p className="errorMessage">{errors.email.message}</p>}
+      <InputField type="password" placeholder="Password" icon="lock"   register={register("password")}  />
         {errors.password && <p className="errorMessage">{errors.password.message}</p>}
-        <input type="submit" value="login" />
-        Or <Link to="/signup">SignUp</Link>
+
+        {errors.password && <p className="errorMessage">{errors.password.message}</p>}
+
+        <a href="#" className="forgot-password-link">Forgot password?</a>
+        <input type="submit" className="login-button" value="Log In"/>
       </form>
+      <p className="signup-prompt">
+      
+   
+        Don&apos;t have an account? <Link to="/signup">Sign up</Link>
+      </p>
+    </div>
     </div>
   );
 }
--- client/src/componants/login/Singup.js
+++ client/src/componants/login/Singup.js
@@ -2,6 +2,8 @@ import axios from '../../axios';
 import { useForm } from "react-hook-form";
 import { useNavigate, Link } from "react-router-dom";
 import "./login.css";
+import SocialLogin from './SocialLogin';
+import InputField from './InputField';
 
 function Signup() {
   const navigate = useNavigate();
@@ -35,25 +37,51 @@ function Signup() {
   }
 
   return (
-    <div className="signup">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <h1>Sign Up</h1>
-        <fieldset>
-          <legend><span className="number">1</span> Your basic info</legend>
-          <label>Name:</label>
-          <p className="errorMessage">{errors.username?.message}</p>
-          <input type="text" {...register("username", { required: "Name required" })} />
-          <label>Email:</label>
-          <p className="errorMessage">{errors.email?.message}</p>
-          <input type="email" {...register("email", { required: "Email required", pattern: { value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/, message: "Email invalid" } })} />
-          <label>Password:</label>
-          <p className="errorMessage">{errors.password?.message}</p>
-          <input type="password" {...register("password", { required: "Password required" })} />
-        </fieldset>
-        <input type="submit" value="SignUp" />
-        Or <Link to="/">Login page</Link>
-      </form>
-    </div>
+    <div className='loginPage'>
+    <div className="login-container">
+    <h2 className="form-title">Sign up with</h2>
+    <SocialLogin />
+    <p className="separator"><span>or</span></p>
+    <form action="#" className="login-form" onSubmit={handleSubmit(onSubmit)}>
+    <InputField type="text" placeholder="Username" icon="person"  register={{...register("username", { required: "Name required" })}}  />
+    { errors.username && <p className="errorMessage">{errors.username?.message}</p>}
+
+    <InputField type="email" placeholder="Email address" icon="mail"  register={ {...register("email", { required: "Email required", pattern: { value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/, message: "Email invalid" } })}}  />
+    { errors.email && <p className="errorMessage">{errors.email?.message}</p>}
+
+
+    <InputField type="password" placeholder="Password" icon="lock"   register={register("password")}  />
+      {errors.password && <p className="errorMessage">{errors.password.message}</p>}
+
+      <a href="#" className="forgot-password-link">Forgot password?</a>
+      <input type="submit" className="login-button" value="Log In"/>
+    </form>
+    <p className="signup-prompt">
+    
+ 
+      Do you have an account? <Link to="/">Sign in</Link>
+    </p>
+  </div>
+  </div>
+    // <div className="signup">
+    //   <form onSubmit={handleSubmit(onSubmit)}>
+    //     <h1>Sign Up</h1>
+    //     <fieldset>
+    //       <legend><span className="number">1</span> Your basic info</legend>
+    //       <label>Name:</label>
+    //       <p className="errorMessage">{errors.username?.message}</p>
+    //       <input type="text" {...register("username", { required: "Name required" })} />
+    //       <label>Email:</label>
+    //       <p className="errorMessage">{errors.email?.message}</p>
+    //       <input type="email" {...register("email", { required: "Email required", pattern: { value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/, message: "Email invalid" } })} />
+    //       <label>Password:</label>
+    //       <p className="errorMessage">{errors.password?.message}</p>
+    //       <input type="password" {...register("password", { required: "Password required" })} />
+    //     </fieldset>
+    //     <input type="submit" value="SignUp" />
+    //     Or <Link to="/">Login page</Link>
+    //   </form>
+    // </div>
   );
 }
 
